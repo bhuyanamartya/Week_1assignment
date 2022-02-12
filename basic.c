27e4f5369d3838a5982b74431b472bf00d28a8dc
bhuyanamartya <99415664+bhuyanamartya@users.noreply.github.com>
2022-02-12 19:24:51 +0530

    Basic file yploaded

---
+++ basic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char n[100], h[100], b[100];
+int r;
+printf("Enter your name: \n");
+gets(n);
+printf("\n Enter your hobby: \n");
+gets(h);
+printf("\n Enter your branch: \n");
+gets(b);
+printf("\n Enter your registration number: \n");
+scanf("%d", &r);
+return 0;
+}
